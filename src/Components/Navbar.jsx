dee3ffe14a5e92ffe924e27a037549752f8f3181
shrutirthr29 <shray.shruti@gmail.com>
2024-06-09 15:50:23 +0530

    image rendering issue resolved

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
         <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="#home" class="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="src/assets/logo.png" class="h-8" alt="Flowbite Logo"/>
+                    <img src="public/assets/logo.png" class="h-8" alt="Flowbite Logo"/>
                         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">uifry™</span>
                 </a>
                 <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
