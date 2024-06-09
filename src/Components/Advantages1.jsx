932e676f27548189f377a14ecfe6e7d6c95db6ad
shrutirthr29 <shray.shruti@gmail.com>
2024-06-09 15:54:50 +0530

    issue resolved

--- src/Components/Advantages1.jsx
+++ src/Components/Advantages1.jsx
@@ -8,13 +8,13 @@ export default function Advantages1() {
                     <h3 class="mb-0 text-4l tracking-wider font-bold text-red-500 dark:text-white">ADVANTAGES</h3>
                     <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Why Choose Uifry?</h2>
                     <div class="features-list flex items-center gap-4 mb-6 mt-6">
-                        <img src='src/assets/notifi.png' class=""></img>
+                        <img src='/assets/notifi.png' class=""></img>
                         <h2 class="text-xl tracking-tight font-bold text-gray-900 dark:text-white">Clever Notifications</h2>
                     </div>
                     <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Arcu At Dictum Sapien, Mollis. Vulputate Sit Id Accumsan, Ultricies. In Ultrices Malesuada Elit Mauris Etiam Odio. Duis Tristique Lacus, Et Blandit Viverra Nisl Velit. Sed Mattis Rhoncus, Diam Suspendisse Sit Nunc, Gravida Eu. Lectus Eget Eget Ac Dolor Neque Lorem Sapien, Suspendisse Aliquam.</p>
                 </div>
-                <img class="w-full dark:hidden" src="src/assets/adv.png" alt="dashboard image" />
-                <img class="w-full hidden dark:block" src="src/assets/adv.png" alt="dashboard image" />
+                <img class="w-full dark:hidden" src="/assets/adv.png" alt="dashboard image" />
+                <img class="w-full hidden dark:block" src="/assets/adv.png" alt="dashboard image" />
             </div>
         </section>
     )
